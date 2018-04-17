f00899bd7ec7f7bce42d4702f81078146d65d127
hyunkyuchoi <chlgusrb6@gmail.com>
2018-04-17 10:41:40 +0900

    Nodejs_2018_TestNode

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 global.console.log('Hellow, world!');
 const circle = require('./circle');
 console.log('Area of a circle = '+ circle.area(3));
+console.log('Circumference of a circle = '+ circle.circum(3));
+console.log('PI = '+ circle.pi);
